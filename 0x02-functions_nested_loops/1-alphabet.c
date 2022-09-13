438a90f087ae5fc8a87d169134a0a0ced7ba7fa8
Adewale <adewaleadejumoyusuf@gmail.com>
2022-09-13 13:18:23 -0700

    t1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main"
+
+/**
+ * print_alphabet - the function that prints the alphabets in lowercase
+ * return: 0
+ */
+
+void print_alphabet(void);
+{
+	char p;
+
+	for (p = 'a'; p <= 'z'; p++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}

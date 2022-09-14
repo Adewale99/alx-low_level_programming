107aa0eda36b2c6bb66a61904d572681c7744c50
Adewale <adewaleadejumoyusuf@gmail.com>
2022-09-13 17:15:38 -0700

    t2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Function that prints 10 times the alphabet in lowercase
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char i, p;
+
+	for (i = '0', i <= '9', i++)
+	{
+		for (p = 'a', p <= 'z', p++)
+		{
+			_putchar(p);
+		}
+		_putchar('\n');
+	}
+}

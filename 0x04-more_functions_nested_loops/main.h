cb670fbaa8bdd18f3c0f6a150ddcb978e96d7fcd
Adewale <adewaleadejumoyusuf@gmail.com>
2022-09-16 08:11:29 -0700

    t3

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,5 @@
-
-
-ifndef MAIN_H_
-define MAIN_H_
-
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -17,4 +14,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif 
+#endif

ba30c81b5e66b922352bfa0cc0cc80d198bf24f9
Adewale <adewaleadejumoyusuf@gmail.com>
2022-09-16 19:38:10 -0700

    t12

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+
 #include <stdio.h>
 
 void positive_or_negative(int i);
@@ -10,4 +11,3 @@ int convert_day(int month, int day);
 void print_times_table(int n);
 
 #endif
-

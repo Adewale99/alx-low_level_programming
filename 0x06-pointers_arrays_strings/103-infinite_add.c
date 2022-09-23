23cf46f739247ca53ddf21e47a1147e1047c8e63
Adewale <adewaleadejumoyusuf@gmail.com>
2022-09-22 21:44:14 -0700

    t11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * * infinite_add - adds two numbers.
  * * @n1: First number
@@ -16,6 +17,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	for (i = 0; n1[i]; i++)
 		;
+	for (j = 0; n2[j]; j++)
+		;
 	if (i > size_r || j > size_r)
 		return (0);
 	m = 0;
@@ -29,21 +32,20 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		if (i < 0 && j < 0 && n == 0)
 		{
 			break;
+			m = n / 10;
+			r[k] = n % 10 + '0';
 		}
-		m = n / 10;
-		r[k] = n % 10 + '0';
-	}
-	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
-		return (0);
-	for (k -= 1, l = 0; l < k; k--, l++)
-	{
-		m = r[k];
-		r[k] = r[l];
-		r[l] = m;
+		r[k] = '\0';
+		if (i >= 0 || j >= 0 || m)
+			return (0);
+		for (k -= 1, l = 0; l < k; k--, l++)
+		{
+			m = r[k];
+			r[k] = r[l];
+			r[l] = m;
+		}
 	}
 
 	return (r);
 }
-
 
